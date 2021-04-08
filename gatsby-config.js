ec28b85152adf724320870c9b48f97bfb999b4d1
Carlos Bautista <cbautista2013@gmail.com>
2021-04-07 23:31:22 -0700

    added new plug in

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,6 +106,16 @@ module.exports = {
         display: `standalone`,
         icon: ``
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
     }
   ]
 };
